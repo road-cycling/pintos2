df7addf6a0df96130df86cf4d60638cae97a38c7
nieroda <eathotlead@gmail.com>
2018-10-11 20:04:49 -0700

    checkpoint

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -1,30 +1,30 @@
-#include "userprog/syscall.h"
 #include <stdio.h>
+#include <stdbool.h>
 #include <syscall-nr.h>
+#include "userprog/syscall.h"
+#include "threads/malloc.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
 #include "threads/synch.h"
+#include "threads/thread.h"
 #include "userprog/pagedir.h"
 #include "devices/shutdown.h"
 #include "filesys/filesys.h"
-//#include "lib/kernel/list.h"
+#include "filesys/file.h"
+#include "filesys/off_t.h"
+#include "lib/kernel/list.h"
 //struct lock
 
+
 static void syscall_handler (struct intr_frame *);
-static int write(uint32_t *args);
-static int open(uint32_t *args);
-static void close(uint32_t *args);
-static void exit(uint32_t *args);
-static bool isValidAddr(uint32_t *);
-static void halt(void);
 
 static struct list FD;
-
 
 struct fileDescriptor {
   int fd;
   struct file *file;
+  struct thread *t;
   struct list_elem globalFDList;
   struct list_elem threadFDList;
 };
@@ -69,16 +69,16 @@ static void syscall_handler (struct intr_frame *f UNUSED) {
   } else if (*args == SYS_TELL) {
 
   } else if (*args == SYS_CLOSE) {
-
+    close(args);
   }
 }
 
-static void exit(uint32_t *args) {
+void exit(uint32_t *args) {
   struct thread *cur = thread_current();
   printf("%s: exit(%d)\n", cur->name, *(args + 1));
 }
 
-static int write(uint32_t *args) {
+int write(uint32_t *args) {
 
   if (!isValidAddr((void *) args[2]) || args[1] == 0) { return 0; }
   int fd = (int) args[1];
@@ -91,47 +91,147 @@ static int write(uint32_t *args) {
   return size;
 }
 
-static void close(uint32_t *args) {
+
+/*
+struct fileDescriptor {
+  int fd;
+  struct file *file;
+  struct thread *t;
+  struct list_elem globalFDList;
+  struct list_elem threadFDList;
+};
+*/
+
+void close(uint32_t *args) {
 
   struct thread *t = thread_current();
   int closeFD = (int) args[1];
+  struct fileDescriptor *fdStruct = NULL;
+  //FD lock acquire
+  if (closeHelperThread(closeFD, &t->fdList, t) && (fdStruct = closeHelperGlobal(closeFD, &FD, t)) != NULL) {
+    file_close(fdStruct->file);
+    if (fdStruct->fd < t->lowestOpenFD)
+      t->lowestOpenFD = fdStruct->fd;
+    free(fdStruct);
+  }
+
+  //fs lock release
 }
 
-static int open(uint32_t *args) {
+int open(uint32_t *args) {
 
   const char *name = (char *)args[1];
   int setFD = -1;
   struct file *f = NULL;
 
   if (!isValidAddr((void *)args[1]) && (f = filesys_open(name)) != NULL) {
+
     //Acquire FS lock
     struct thread *t = thread_current();
     struct fileDescriptor *fileDesc = malloc(sizeof(struct fileDescriptor));
     setFD = t->lowestOpenFD++;
 
-    ASSERT(setFD != 0 || setFD != 1);
-
+    //ASSERT(setFD != 0 || setFD != 1);
+    fileDesc->t = t;
     fileDesc->fd = setFD;
     fileDesc->file = f;
 
     list_push_back(&t->fdList, &fileDesc->threadFDList);
     list_push_back(&FD ,&fileDesc->globalFDList);
-    //Release FS lock
 
+    //Release FS lock
   }
+
   return setFD;
+}
+
+
+unsigned tell (uint32_t *args) {
+  int fd = (int) args[1];
+  unsigned nextByte = 0;
+  //acquire FS lock
+  struct file *fp = NULL;
+
+  fp = getFileFromFD(fd, thread_current());
+
+  if (fp != NULL) {
+    nextByte = (unsigned) fp->pos;
+  }
+
+  //release FS lock
+  return nextByte;
+}
+
+int filesize (uint32_t *args) {
+  int fd = (int) args[1];
+  int fileSize = 0;
+
+  struct file *fp = NULL;
+
+  //acquireFS lock
+  if ((fp = getFileFromFD(fd, thread_current())) != NULL) {
+    //fileSize = (int) fp->inode->lenth;
+    fileSize = (int) file_get_inode(fp)->length;
+  }
+
+  //release FS lock
+  return fileSize;
 
 }
 
-static void halt(void) {
+void halt(void) {
   shutdown_power_off();
 }
 
-static bool isValidAddr(uint32_t *vaddr) {
+/* Helper Functions Below */
+
+bool isValidAddr(uint32_t *vaddr) {
 
   struct thread *cur = thread_current();
   //check its a user address
   // int *PHYS_BASE = (int *)0xC0000000;
   // 0xC0000000 > vaddr
   return is_user_vaddr(vaddr) && pagedir_get_page(cur->pagedir,(void *) vaddr);
+}
+
+struct file *getFileFromFD(int fd, struct thread *t) {
+
+  struct list_elem *iter;
+  for (iter = list_begin(&t->fdList); iter != list_end(&t->fdList); iter = list_next(iter)) {
+    struct fileDescriptor *fdStruct = list_entry(iter, struct fileDescriptor, threadFDList);
+    if (fdStruct->fd == fd && fdStruct->t == t)
+      return fdStruct->file;
+  }
+  return NULL;
+}
+
+/* need 2 functions due to
+../../userprog/syscall.c:211: error: ‘struct fileDescriptor’ has no member named ‘global’
+../../userprog/syscall.c:211: error: ‘globalFDList’ undeclared (first use in this function)
+../../userprog/syscall.c:211: error: (Each undeclared identifier is reported only once
+../../userprog/syscall.c:211: error: for each function it appears in.)
+../../userprog/syscall.c:211: error: ‘threadFDList’ undeclared (first use in this function)
+*/
+struct fileDescriptor *closeHelperGlobal(int fd, struct list *lst, struct thread *t) {
+  struct list_elem *iter;
+  for (iter = list_begin(lst); iter != list_end(lst); iter = list_next(iter)) {
+    struct fileDescriptor *fdStruct = list_entry(iter, struct fileDescriptor, globalFDList);
+    if (fdStruct->fd == fd && fdStruct->t == t) {
+      list_remove(&fdStruct->globalFDList);
+      return fdStruct;
+    }
+  }
+  return NULL;
+}
+
+struct fileDescriptor *closeHelperThread(int fd, struct list *lst, struct thread *t) {
+  struct list_elem *iter;
+  for (iter = list_begin(lst); iter != list_end(lst); iter = list_next(iter)) {
+    struct fileDescriptor *fdStruct = list_entry(iter, struct fileDescriptor, threadFDList);
+    if (fdStruct->fd == fd && fdStruct->t == t) {
+      list_remove(&fdStruct->threadFDList);
+      return fdStruct;
+    }
+  }
+  return NULL;
 }
--- userprog/syscall.h
+++ userprog/syscall.h
@@ -1,7 +1,27 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include "threads/thread.h"
+#include "lib/kernel/list.h"
+#include "filesys/off_t.h"
+
 void syscall_init (void);
+
+//helpers
+struct file *getFileFromFD(int fd, struct thread *);
+//struct fileDescriptor *closeHelper(int fd, struct list *, bool, struct thread *);
+struct fileDescriptor *closeHelperThread(int fd, struct list *lst, struct thread *t);
+struct fileDescriptor *closeHelperGlobal(int fd, struct list *lst, struct thread *t);
+bool isValidAddr(uint32_t *);
+
+//core syscall
+int write(uint32_t *args);
+int open(uint32_t *args);
+void close(uint32_t *args);
+void exit(uint32_t *args);
+unsigned tell (uint32_t *);
+int filesize (uint32_t *);
+void halt(void);
 
 
 #endif /* userprog/syscall.h */
@@ -12,7 +32,7 @@ enum    {
   SYS_HALT,
   SYS_EXIT,
   SYS_EXEC,
-  SYS_WAIT, 
+  SYS_WAIT,
   SYS_CREATE,
   SYS_REMOVE,
   SYS_OPEN,
